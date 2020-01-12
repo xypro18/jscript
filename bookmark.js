dc68214dcf2b543a2115bb4b81eb507564d31779
xypro18 <37643655+xypro18@users.noreply.github.com>
2020-01-12 15:55:33 +0000

    Add files via upload

--- bookmark.js
+++ bookmark.js
@@ -1,7 +1,23 @@
 javascript: (function(e, s) {
     e.src = s;
     e.onload = function() {
-		helper.getAllInputValues()
+		helper.getSelectedInputValues("selectors", "input_values");
+    };
+    document.head.appendChild(e);
+})(document.createElement('script'), '//cdn.jsdelivr.net/gh/xypro18/jscript/helper.js')
+
+javascript: (function(e, s) {
+    e.src = s;
+    e.onload = function() {
+		helper.fillAllInputValues("input_values");
+    };
+    document.head.appendChild(e);
+})(document.createElement('script'), '//cdn.jsdelivr.net/gh/xypro18/jscript/helper.js')
+
+javascript: (function(e, s) {
+    e.src = s;
+    e.onload = function() {
+		helper.getAllInputValues("input_values");
     };
     document.head.appendChild(e);
 })(document.createElement('script'), '//cdn.jsdelivr.net/gh/xypro18/jscript/helper.js')
--- helper.js
+++ helper.js
@@ -1,27 +1,36 @@
 var helper = {
-	keys: [
-	{
-		id: "#teste",
-		type: null
-	}		
-	],	
-	getAllInputValues() {
-		var list = document.querySelectorAll("input[id]");
-		var results = []
-		for (var item of list) {
+	getInputValues(elements, storageValues) {
+		var results = [];
+		for (var item of elements) {
 			if (item.value != "")
 				results.push({
 					selector: "input" + "#" + item.id,
 					value: item.value
 				});
 		}
-		localStorage.setItem('input_values', JSON.stringify(results))
-	},		
-	fillAllInputValues() {
-		var list = JSON.parse(localStorage.getItem('input_values'));
+		localStorage.setItem(storageValues, JSON.stringify(results));
+	},
+	getAllInputValues(storageValues) {
+		this.getInputValues(document.querySelectorAll("input[id]"), storageValues);
+	},
+	getSelectedInputValues(storageSelectors, storageValues) {
+		var list = JSON.parse(localStorage.getItem(storageSelectors));
+		var elements = list.map(item => document.querySelector(item));
+		this.getInputValues(elements, storageValues);
+	},
+	fillAllInputValues(storageValues) {
+		var list = JSON.parse(localStorage.getItem(storageValues));
 		for (var item of list) {
 			elem = document.querySelector(item.selector);
 			elem.value = item.value
 		}
+	},
+	storestorageSelectors(storageValues, storageSelectors) {
+		var list = JSON.parse(localStorage.getItem(storageValues));
+		var results = [];
+		for (var item of list) {
+			results.push(item.selector)
+		}
+		localStorage.setItem(storageSelectors, JSON.stringify(results))	
 	}
 }
